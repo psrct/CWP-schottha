4722b54a55167a5025f23995405e14492b09f96c
Sirachet Chotthakunanan <sirachet.c@gmail.com>
2026-09-21 21:21:51 +0700

    cell01-ex2-3

---
+++ cell01/ex02/age.py
@@ -0,0 +1,2 @@
+my_age = 21 + 42
+print(my_age)
---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,4 @@
+firstname = input("Hey, what's your first name? : ")
+lastname = input("And your last name? : ")
+result = f"Well, pleased to meet you, {firstname} {lastname}"
+print(result)
