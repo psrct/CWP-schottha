4ee2d4981b1f03da6237b04306148715d7dbb41d
Sirachet Chotthakunanan <sirachet.c@gmail.com>
2026-09-21 21:21:06 +0700

    cell1

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Sirachet"
+last_name = "Chotthakunanan"
+print(f"{first_name} {last_name}")
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Sirachet"
+last_name = "Chotthakunanan"
+whole_name = f"{first_name} {last_name}"
+print(whole_name)
